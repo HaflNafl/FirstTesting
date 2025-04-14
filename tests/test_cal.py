0ada013cc8426f0546370cdea78abd2119074f96
haflnafl <makambaevbael5@gmail.com>
2025-04-14 12:23:53 +0300

    secong

--- tests/test_cal.py
+++ tests/test_cal.py
@@ -1,38 +1,54 @@
+import pytest
 from src.calc import Calculator
 
 calc = Calculator()
 
-def test_addition():
-    assert calc.sum(2,2) == 4
-    assert calc.sum(2.1, 2.1) == 4.2
-    assert calc.sum(2/1,2) == 4
-    assert calc.sum(-2,-2) == -4
-    assert calc.sum(8.9,2) == 10.9
-    assert calc.sum(-2,2) == 0
-    assert calc.sum(2**2,2) == 6
-    assert calc.sum(2/9,2/3) == 0.8888888888888888
-    
-def test_sub():
-    assert calc.subtract(2,2) == 0
-    assert calc.subtract(2.1,2.1) == 0
-    assert calc.subtract(2/1,2/1) == 0
-    assert calc.subtract(2**3,2**3) == 0
-    assert calc.subtract(1.2,1.2) == 0
-    assert calc.subtract(0/9,0/9) == 0
-    
-def test_mult():
-    assert calc.multiply(2,2) == 4
-    assert calc.multiply(1/1,2) == 2.0
-    assert calc.multiply(2**2, 45**1) == 180
-    assert calc.multiply(-2, -1/2) == 1
-    assert calc.multiply(-3**1,-2.4) == 7.199999999999999
-    assert calc.multiply(7/1, -2.3**3) == -85.16899999999998
+@pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (1, 2, 3),
+            (-4, 4**2, 12),
+            (1/3, -2**4, -15.666666666666666 )
+        ]
+)
+def test_addition(a, b, expected ):
+    assert calc.sum(a,b) == expected
 
-def test_div():
-    assert calc.divide(2,2) == 1
-    assert calc.divide(2.1,2/2) == 2.1
-    assert calc.divide(-2,-1.2) == 1.6666666666666667
-    assert calc.divide(5**2,-2) == -12.5
-    assert calc.divide(2/2,2.0) == 0.5
-    assert calc.divide(2**7,-9.4**3/6) == -0.9246506072835499
+@pytest.mark.parametrize(
+        "c, s, expected",
+        [
+            (2, 5, -3),
+            (-9, 7**7, -823552),
+            (1/2, 9.5, -9.0)
+        ]
+
+) 
+def test_sub(c, s, expected):
+    assert calc.subtract(c,s) == expected
+    
+
+@pytest.mark.parametrize(
+        "d, k, expected",
+        [
+            (3, 6, 18 ),
+            (-5, 5., -25.0),
+            (6/3, 3**4, 162.0)
+        ]
+)
+
+def test_mult(d, k, expected):
+    assert calc.multiply(d,k) == expected
+
+@pytest.mark.parametrize(
+        "o, m, expected",
+        [
+            (4, 4, 1),
+            (2.2, -6/2, -0.7333333333333334),
+            (5.3**3, -91, -1.6360109890109888)
+        ]
+)
+
+
+def test_div(o,m,expected):
+    assert calc.divide(o,m) == expected
     
